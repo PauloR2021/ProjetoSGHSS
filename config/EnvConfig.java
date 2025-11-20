9cce94a211745de3136fec78e5d081bba8b32985
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-11-20 20:01:09 -0300

    -exit  Please enter the commit message for your changes. Lines starting -exit

---
+++ config/EnvConfig.java
@@ -0,0 +1,15 @@
+import org.springframework.context.annotation.Configuration;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@Configuration
+public class EnvConfig {
+
+    public static final Dotenv dotenv = Dotenv.configure()
+            .ignoreIfMissing()
+            .load();
+
+    public static String get(String key) {
+        return dotenv.get(key);
+    }
+}
