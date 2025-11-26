19a94dcd315e3bf318a75cf4ff8365bea4c327fa
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-11-26 20:18:39 -0300

    Adicioando as classes de config da aplicacao

---
+++ config/JwtFilter.java
@@ -0,0 +1,46 @@
+package com.pauloricardo.sghss.util;
+
+import com.pauloricardo.sghss.util.JwtUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+            try {
+                Claims claims = jwtUtil.validate(token).getBody();
+                String username = claims.getSubject();
+                List<String> roles = claims.get("roles", List.class);
+                var auth = new UsernamePasswordAuthenticationToken(
+                        username,
+                        null,
+                        roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            } catch (Exception e) {
+                // invalid token
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
